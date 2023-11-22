6104fd493eabc2628f012d2fea027ba3f2933dc2
Myers Mbonda <mmbonda@lcs.on.ca>
2023-11-21 19:06:40 -0500

    Finished the socks interface fully.

--- Stocks/ContentView.swift
+++ Stocks/ContentView.swift
@@ -21,22 +21,25 @@ struct ContentView: View {
                             Image(systemName:"chevron.up.chevron.down").foregroundColor(.blue)
                         }
                         Group {
-                            Stock(shortCompany: "AAPL", companyName: "Apple Inc.", graph: "greenwhite", price: "$188.01", percentage: "↑2.34%", rectangleColor: .green).listRowSeparator(.hidden)
+                            Stock(shortCompany: "AAPL", companyName: "Apple Inc.", graph: "greenwhite", price: "188.01", percentage: "↑2.34%", rectangleColor: .green).listRowSeparator(.hidden)
                         
                         
                         
-                        Stock(shortCompany: "SPOT", companyName: "Spotify  S.A.", graph: "redwhite", price: "$173.76", percentage:"↓1.02%", rectangleColor: .red).listRowSeparator(.hidden)
-                        Stock(shortCompany: "AMZN", companyName: "Amazon Inc.", graph: "greenwhite", price: "$143.20", percentage: "↑1.78%", rectangleColor: .green).listRowSeparator(.hidden)
-                        Stock(shortCompany: "GOOGL", companyName: "Alphabet Inc.", graph: "greenwhite", price: "$134.62", percentage: "↑3.21%", rectangleColor: .green).listRowSeparator(.hidden)
-                        Stock(shortCompany: "NKE", companyName: "Nike Inc.", graph: "redwhite", price: "$107.82", percentage: "↓1.93%", rectangleColor: .red).listRowSeparator(.hidden)
+                        Stock(shortCompany: "SPOT", companyName: "Spotify  S.A.", graph: "redwhite", price: "173.76", percentage:"↓1.02%", rectangleColor: .red).listRowSeparator(.hidden)
+                        Stock(shortCompany: "AMZN", companyName: "Amazon Inc.", graph: "greenwhite", price: "143.20", percentage: "↑1.78%", rectangleColor: .green).listRowSeparator(.hidden)
+                        Stock(shortCompany: "GOOGL", companyName: "Alphabet Inc.", graph: "greenwhite", price: "134.62", percentage: "↑3.21%", rectangleColor: .green).listRowSeparator(.hidden)
+                        Stock(shortCompany: "NKE", companyName: "Nike Inc.", graph: "redwhite", price: "107.82", percentage: "↓1.93%", rectangleColor: .red).listRowSeparator(.hidden)
                         Stock(shortCompany: "TSLA", companyName: "Tesla Inc.", graph: "greenwhite", price: "242.82", percentage: "↑5.43%", rectangleColor: .green).listRowSeparator(.hidden)
-                            Stock(shortCompany: "WMT", companyName: "Walmart Inc", graph: "greenwhite", price: "$169.78", percentage: "↑0.33%", rectangleColor: .green)
+                            Stock(shortCompany: "WMT", companyName: "Walmart Inc", graph: "greenwhite", price: "169.78", percentage: "↑0.33%", rectangleColor: .green).listRowSeparator(.hidden)
+                            Stock(shortCompany: "INTC", companyName: "Intel Corp", graph: "redwhite", price: "41.32", percentage: "↓0.93%", rectangleColor: .red).listRowSeparator(.hidden)
+                            Stock(shortCompany: "AC", companyName: "Air Canada", graph: "redwhite", price: "18.09", percentage: "↓0.93%", rectangleColor: .red)
+                            
                         
                         
                     }
                     
                 }
-                    .navigationTitle("Stocks")
+                    .navigationTitle("My Stocks")
                     
                     .listStyle(.plain)
                     .searchable(text: .constant(""),prompt: "Search")
